cc9995c5bd59d66ddcbdeb7de011c936f79d6cc4
sweisz <dev@steffenweisz.de>
2021-02-15 13:50:42 +0100

    Changed styling

--- src/components/card/card.ts
+++ src/components/card/card.ts
@@ -1,8 +1,20 @@
 import { createElement } from "../../utils/createElement";
 
 export function createCard({ imgSrc }) {
-  return createElement("img", {
-    className: "catImage",
-    src: imgSrc,
+  return createElement("div", {
+    className: "container",
+    childs: [
+      createElement("h2", {
+        innerText: "A random cat",
+      }),
+      createElement("img", {
+        className: "catImage",
+        src: imgSrc,
+      }),
+      createElement("button", {
+        className: "btn",
+        innerText: "Next Cat",
+      }),
+    ],
   });
 }
